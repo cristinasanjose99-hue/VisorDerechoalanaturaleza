331471bb240a768cc7389ae31ef68973afc20711
cristinasanjose99-hue <cristinasanjose99@gmail.com>
2026-04-07 20:02:04 +0200

    Add files via upload

---
+++ qgis2web_2026_04_07-19_55_38_547910/styles/Mapabivariado_3_style.js
@@ -0,0 +1,105 @@
+var size = 0;
+var placement = 'point';
+function categories_Mapabivariado_3(feature, value, size, resolution, labelText,
+                       labelFont, labelFill, bufferColor, bufferWidth,
+                       placement) {
+                var valueStr = (value !== null && value !== undefined) ? value.toString() : 'default';
+                switch(valueStr) {case '11':
+                    return [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(174,174,174,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '12':
+                    return [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(88,152,112,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '13':
+                    return [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(1,130,49,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '21':
+                    return [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(140,109,152,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '22':
+                    return [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(70,95,97,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '23':
+                    return [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(1,82,43,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '31':
+                    return [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(106,44,129,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '32':
+                    return [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(53,39,83,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '33':
+                    return [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(1,33,36,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;}};
+
+var style_Mapabivariado_3 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("Bivariado");
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    
+    var style = categories_Mapabivariado_3(feature, value, size, resolution, labelText,
+                            labelFont, labelFill, bufferColor,
+                            bufferWidth, placement);
+
+    return style;
+};
