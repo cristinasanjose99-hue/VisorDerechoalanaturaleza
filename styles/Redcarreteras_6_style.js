987821e004818e5d6c77f41f622b7b99427551d0
cristinasanjose99-hue <cristinasanjose99@gmail.com>
2026-04-07 20:04:58 +0200

    Add files via upload

---
+++ styles/Redcarreteras_6_style.js
@@ -0,0 +1,105 @@
+var size = 0;
+var placement = 'point';
+function categories_Redcarreteras_6(feature, value, size, resolution, labelText,
+                       labelFont, labelFill, bufferColor, bufferWidth,
+                       placement) {
+                var valueStr = (value !== null && value !== undefined) ? value.toString() : 'default';
+                switch(valueStr) {case 'Autopista libre / autovía':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(255,255,255,0.85)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 1.52}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Camino':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(255,255,255,0.85)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0.38}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Carretera convencional':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(255,255,255,0.85)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0.95}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Carretera multicarril':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(255,255,255,0.85)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0.57}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Carril bici':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(255,255,255,0.85)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0.38}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Senda':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(255,255,255,0.85)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0.38}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Urbano':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(238,238,238,0.85)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0.38}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Urbano diseminado':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(255,255,255,0.85)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0.38}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'NULL':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(225,94,181,0.85)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0.38}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;}};
+
+var style_Redcarreteras_6 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("clased");
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'line';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    
+    var style = categories_Redcarreteras_6(feature, value, size, resolution, labelText,
+                            labelFont, labelFill, bufferColor,
+                            bufferWidth, placement);
+
+    return style;
+};
